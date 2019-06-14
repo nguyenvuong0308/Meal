21bc2b1118922c3b3c065033956221ee9365c628
Minh Vuong <vuong.nguyen@ntq-solution.com.vn>
2019-06-14 17:35:04 +0700

    init

---
+++ CustomTableViewCell/RatingControl.swift
@@ -0,0 +1,89 @@
+//
+//  RatingControl.swift
+//  CustomTableViewCell
+//
+//  Created by NTQ on 6/14/19.
+//  Copyright © 2019 NTQ. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class RatingControl: UIStackView {
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    @IBInspectable var startSize: CGSize = CGSize(width: 44.4, height: 44.4) {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var startCount: Int = 5
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private method
+    private func setupButtons() {
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        // Load Button Images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "star_fill", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "star_empty", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "star_highlighted", in: bundle, compatibleWith: self.traitCollection)
+        for _ in 0..<startCount {
+            let buttonRed = UIButton()
+            buttonRed.translatesAutoresizingMaskIntoConstraints = false
+            buttonRed.heightAnchor.constraint(equalToConstant: startSize.height).isActive = true
+            buttonRed.widthAnchor.constraint(equalToConstant: startSize.width).isActive = true
+            buttonRed.setImage(emptyStar, for: .normal)
+            buttonRed.setImage(filledStar, for: .selected)
+            buttonRed.setImage(highlightedStar, for: .highlighted)
+            buttonRed.setImage(highlightedStar, for: [.highlighted, .selected])
+            buttonRed.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            addArrangedSubview(buttonRed)
+            ratingButtons.append(buttonRed)
+        }
+        updateButtonSelectionStates()
+    }
+    
+    @objc func ratingButtonTapped(button: UIButton) {
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
+    }
+    
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
+    }
+    
+}
--- CustomTableViewCell/ViewController.swift
+++ CustomTableViewCell/ViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var tfMeal: UITextField!
+    @IBOutlet weak var lblMeal: UILabel!
+    @IBOutlet weak var ivMeal: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    //MARK: Actions
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        tfMeal.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        tfMeal.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tfMeal.delegate = self
     }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        tfMeal.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        lblMeal.text = tfMeal.text
+    }
+    
+    //MARK: UIImagePickerController
+    
 
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        ivMeal.image = selectedImage
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
